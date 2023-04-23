9f91495833fd0530de60756e1c7aacc37bf81211
yuuu <okazaki@fusic.co.jp>
2023-04-23 13:40:22 +0900

    Add users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
+  end
 end
